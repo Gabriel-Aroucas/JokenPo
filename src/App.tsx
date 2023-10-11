7478e06895d07347dbef929979eb93ad26f17d4e
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-10-11 14:38:23 -0300

    :recycle: refactor: using dinamic data

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,72 @@
-import './App.css'
-import HandRock from './assets/HandRock.png'
-import Hand from './assets/Hand.png'
-import HandScissors from './assets/HandScissors.png'
-import Unknown from './assets/unknown.png'
-import { useState,useEffect } from 'react'
-
+import "./App.css";
+import HandRock from "./assets/HandRock.png";
+import Hand from "./assets/Hand.png";
+import HandScissors from "./assets/HandScissors.png";
+import Unknown from "./assets/unknown.png";
+import { useState } from "react";
 const App = () => {
- 
-  const [choice, setchoice] = useState(Unknown);
-  const [playerOne, setPlayerOne] = useState(Unknown);
-  const [playerTwo, setPlayerTwo] = useState(Unknown);
-  const options = [HandRock,Hand,HandScissors]
 
-  const [toggleButton,settoggleButton] = useState(true)
+  const [choice, setchoice] = useState(Unknown)
+  const menuOfChoice = [
+    {
+      id:'1',
+      image: HandRock,
+      title: "Hand Rock",
+      alt: "An image of a Hand Rock",
+      click: () => setchoice(HandRock),
+    },
+    {
+      id:'2',
+      image: Hand,
+      title: "Hand Open",
+      alt: "An image of a Hand Open",
+      click: () => setchoice(Hand),
+
+    },
+    {
+      id:'3',
+      image: HandScissors,
+      title: "Hand Scissor",
+      alt: "An image of a Hand Scissor",
+      click: () => setchoice(HandScissors),
+
+    },
+  ];
+
   
-
-  useEffect(()=>{
-    setPlayerOne(choice)
-  },[choice])
-
-  const validateToPlayGame = ()=>{
-    if(choice == Unknown){
-      alert('Escolha uma opção')
-    }else if(toggleButton == true){
-      playGame()
-    }
-  }
-
-  const playGame = ()=>{
-    settoggleButton(false)
-
-    const animationGame = setInterval(()=>{
-      const randomize = Math.floor(Math.random()*3);
-      setPlayerTwo(options[randomize])
-    },300)
-
-    setTimeout(() => {
-      clearInterval(animationGame)
-      settoggleButton(true)
-      
-    }, 5000);
-    
-    
-    
-  }
-
-
   return (
-      <>
-      <header className='score'>
+    <>
+      <header className="score">
         <span>0</span>
         <span>x</span>
         <span>0</span>
       </header>
 
-      <section className='modalOfPlayers' >
+      <section className="modalOfPlayers">
         <div className="modalPlayerOne">
-          <img src={playerOne} width={'250px'} alt="the choice player image" />
+          <img src={choice} width={"250px"} alt="the choice player image" />
         </div>
         <div>
           <span>x</span>
         </div>
         <div className="modalPlayerTwo">
-          <img src={playerTwo} width={'250px'} alt="the choice player image" />
+          <img src={Unknown} width={"250px"} alt="the choice player image" />
         </div>
       </section>
 
       <section className="menuOfChoice">
-        <img src={HandRock} onClick={()=> setchoice(HandRock)} alt="icons to choice" />
-        <img src={Hand} onClick={()=>setchoice(Hand)} alt="icons to choice" />
-        <img src={HandScissors} onClick={()=> setchoice(HandScissors)} alt="icons to choice" />
+        {menuOfChoice.map((e) => {
+          return (
+            <img src={e.image} title={e.title} alt={e.alt} onClick={e.click} key={e.id} />
+          );
+        })}
       </section>
 
       <section className="buttonPlayGame">
-        <button type='button' onClick={validateToPlayGame}>Jogar</button>
+        <button type="button" >Jogar</button>
       </section>
-      </>    
-  )
-}
+    </>
+  );
+};
 
-export default App
+export default App;
