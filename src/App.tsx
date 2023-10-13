0e43262beca3d31e6e35deb3bdae310182695bc7
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-10-13 18:49:39 -0300

    feat:added validations

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => {
            <li key={e.id}> <img src={e.image} title={e.title} alt={e.alt} onClick={e.click}  /></li>
         )
 
-        useEffect(()=>{
+useEffect(()=>{
             validate()
         })
 
@@ -46,6 +46,18 @@ const validate =()=>{
     const buttonDisabled  = document.querySelector('button')?.setAttribute('disabled','');
     const validate_options = {
         invalid_options:choice &&choiceP2 == Unknown ? buttonDisabled :'',
+
+        rock_win: choice == HandRock && choiceP2 == HandScissors ? alert('win') : '',
+        rock_drawn: choice == HandRock && choiceP2 == HandRock ? alert('drawn') : '',
+        rock_loose: choice == HandRock && choiceP2 == Hand ? alert('loose') : '',
+
+        hand_win: choice == Hand && choiceP2 == HandRock ? alert('win') : '',
+        hand_drawn: choice == Hand && choiceP2 == Hand ? alert('drawn') : '',
+        hand_loose: choice == Hand && choiceP2 == HandScissors ? alert('loose') : '',
+
+        scissor_win: choice == HandScissors && choiceP2 == Hand ? alert('win') : '',
+        scissor_drawn: choice == HandScissors && choiceP2 == HandScissors ? alert('drawn') : '',
+        scissor_loose: choice == HandScissors && choiceP2 == HandRock ? alert('loose') : '',
     }
     validate_options
     
