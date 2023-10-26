72c1db3e1f22275903bdb64d0ce5dcf0ef37a2e6
Gabriel Aroucas <gabrielaroucas4@gmail.com>
2023-10-26 20:12:30 -0300

    feat: added more features

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { styled } from "styled-components"
+
+const Section  = styled.section`
+  border:1px solid red;
+`
 
 const Index = () => {
   return (
-    <div>Index</div>
+    <Section>
+      <div className="title">
+        <h1>bem vindo ao joken po</h1>
+        <img src="https://placehold.co/250x250" alt="alt lorem" />
+      </div>
+      <div className="selectGae">
+        <p>escolha uma opção sei lá</p>
+        <button>Modo 1 Jogador</button>
+        <button>Modo 2 Jogadores</button>
+        <button>Modo Online</button>
+      </div>
+    </Section>
   )
 }
 
